eb0a57ae6680556296791cf9df74cc3b237e5b07
Aqel Ahammad <aqel123@gmail.com>
2015-08-20 18:37:39 +0530

    initial setup

---
+++ plugins/cordova-plugin-chrome-apps-navigation/README.md
@@ -0,0 +1,15 @@
+# Chrome App Navigation override
+
+This plugin contains the code used to intercept navigation within a Chrome app,
+and open links in the system browser instead.
+
+# Release Notes
+
+## 1.0.3 (April 30, 2015)
+- Renamed plugin to pubilsh to NPM
+
+## 1.0.2 (October 21, 2014)
+- Documentation updates.
+
+## 1.0.1 (March 10, 2014)
+- Fixes #76 - Don't detect hash changes as page reloads.
---
+++ www/js/controllers/loginctrl.js
@@ -0,0 +1,55 @@
+var app = angular.module('MyApp', ['ngRoute', 'ngMaterial']);
+
+var server = 'http://10.21.208.212:9000/';
+
+app.config(function ($routeProvider){
+  $routeProvider
+  .when('/index',
+  {
+    controller:'CoreCtrl',
+    templateUrl:'views/core.html'
+    })
+  .when('/login',
+  {
+    controller:'LoginCtrl',
+    templateUrl:'views/login-material.html'
+    })
+  .otherwise({ redirectTo:'/login'});
+
+});
+
+app.controller('LoginCtrl', function($scope, $http, $location) {
+  $scope.login = function(){
+    var username = $scope.username;
+    var password = $scope.password;
+    if (username && password){
+      $http.post( server + 'auth/local/admin/', 
+        {rollNumber: username, password : password}).
+      then(function(response) {
+        // this callback will be called asynchronously
+        // when the response is available
+        console.log(response.data);
+        $scope.role = response.data.user.role.name;
+        // userService.saveUser(response.data.user);
+        $location.path('index');
+      }, function(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        alert(response.data.message);
+    });
+    } else {
+      alert("Don't leave fields empty");
+    }
+  }
+});
+
+app.controller('CoreCtrl', function($scope, $http, $location, $mdSidenav) {
+  console.log("CoreCtrl");
+  $scope.openSideNavPanel = function() {
+    $mdSidenav('left').open();
+  };
+  $scope.closeSideNavPanel = function() {
+    $mdSidenav('left').close();
+  };
+  $scope.action = 'addClub';
+});
